fd44cb884c918458fcaf799ce9be6181e7e63502
Ace Pasag <acepasag100@outlook.com>
2024-04-07 16:33:02 +0800

    Update and Delete added

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace EntityFrameworkSqlite
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EntityFrameworkSqlite
 {
     internal class Program
     {
@@ -6,17 +8,46 @@
         {
            using(var database = new PersonContext())
             {
-                /*database.Add(new Person()
+                /*var per = new Person()
                 {
-                    Name = "Ace"
-                });
-                */
+                    Name = "John"
+                };
+                database.Persons.Add(per);
+
+                var pers = new List<Person>()
+                {
+                    new Person() {Name = "Vic"},
+                    new Person() {Name = "Gordon"},
+                    new Person() {Name = "Emz"}
+                };
+                database.AddRange(pers);
+                database.Add(new Person()
+                {
+                    Name = "Jonalyn"
+                });*/
+
+                /*
                 database.Add(new Table_Three()
                 {
                     Name = "Ace"
-                });
+                });*/
+
+                /*var per = database.Persons.First<Person>(); // First Record
+                 * per.Name = "Ace Pasag";
+                 */
+
+                //var per_Update = database.Persons.First(a => a.Id == 2);
+                //per_Update.Name = "Jonalyn Opino";
+
+                var per_Delete = database.Persons.First(a => a.Id == 6);
+                database.Persons.Remove(per_Delete);
+
                 database.SaveChanges();
+
+                //for (int i = 0; )
             }
+
+           // List Data
 
             Console.WriteLine("Done");
         }
